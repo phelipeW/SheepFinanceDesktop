572b9502757b431eb648a79108508ebbadd7d38e
Abel <abel.o.d@outlook.com>
2019-04-14 18:19:53 -0300

    - Criados modelos de persistencia

---
+++ src/Model/Entrada.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author abelo
+ */
+public class Entrada extends Transacao {
+    public Conta Conta;
+    
+    public Entrada(Date data, Conta conta, Double valor){
+        Descricao = conta.getNome();
+        super.Data = data;
+        super.Valor = valor;
+        Conta = conta;
+    }
+    
+    @Override
+    public void Movimentar(Double valor) {
+        Conta.Credito(valor);
+    }
+    
+}
